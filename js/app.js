0d5446c54ae56f32e62d8f887ea1b23dd5e5fa1d
Eric Tian <tianxing0923@qq.com>
2016-04-22 15:07:11 +0800

    修改时间指令插件，移除时间UTC处理

--- js/app.js
+++ js/app.js
@@ -405,7 +405,11 @@ ipcApp.directive('ngDatetime', ['$compile', function ($compile) {
             if (!$ngModel) {
                 return;
             }
-            $element.datetimepicker();
+            return $element.datetimepicker().on('dp.change', function (e) {
+                $scope.$apply(function () {
+                    $ngModel.$setViewValue(e.date.format('YYYY-MM-DD HH:mm:ss'));
+                });
+            });
         }
     };
 }]);
